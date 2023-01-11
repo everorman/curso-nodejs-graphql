1b0b52eb739948435daacddc1ee760bd20375375
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-01-11 16:18:10 -0500

    Conecting resolver with product service

--- src/graphql/product.resolvers.js
+++ src/graphql/product.resolvers.js
@@ -1,15 +1,15 @@
-const getProduct = (_, { id }) => {
-  return {
-    id: id,
-    name: 'Demo produc',
-    price: 100,
-    image: "http://image.jpg",
-    createdAt: new Date().toISOString()
-  }
+
+const ProductsService = require('./../services/product.service');
+const service = new ProductsService();
+
+const getProduct = async (_, { id }) => {
+  const product = await service.findOne(id);
+  return product;
 }
 
-const getProducts = (_, args) => {
-  return [];
+const getProducts = async (_, args) => {
+  const products = await service.find({});
+  return products;
 }
 
 module.exports = { getProduct, getProducts };
